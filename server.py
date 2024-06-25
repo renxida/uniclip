1d0eddd7f6d91f177c59d59f6229fa78a6defa88
Xida Ren <cedar.ren@gmail.com>
2024-06-24 18:31:41 -0700

    more debug messaging

--- server.py
+++ server.py
@@ -10,24 +10,30 @@ class Server:
         self.pending_updates = {}
         self.app = Flask(__name__)
         self.setup_routes()
+        self.logger.info("Server initialized")
 
     def setup_routes(self):
         self.app.add_url_rule('/register', 'register', self.handle_register, methods=['POST'])
         self.app.add_url_rule('/update', 'update', self.handle_update, methods=['POST'])
         self.app.add_url_rule('/poll/<group_id>', 'poll', self.handle_poll, methods=['GET'])
+        self.logger.info("Routes set up")
 
     def run(self):
         self.logger.info("Running server on port 2547")
         self.db_manager.init_db()
+        self.logger.info("Database initialized")
         serve(self.app, host='0.0.0.0', port=2547)
 
     def handle_register(self):
         data = request.json
         group_id = data.get('group_id')
+        self.logger.debug(f"Received registration request for group: {group_id}")
         if group_id not in self.clients:
             self.clients[group_id] = []
+            self.logger.debug(f"Created new group: {group_id}")
         self.clients[group_id].append(request.remote_addr)
         self.logger.info(f"Client registered: {request.remote_addr} in group {group_id}")
+        self.logger.debug(f"Current clients in group {group_id}: {self.clients[group_id]}")
         return jsonify({"status": "registered"}), 200
 
     def handle_update(self):
@@ -36,25 +42,40 @@ class Server:
         content = data.get('content')
         sender_ip = request.remote_addr
         
+        self.logger.debug(f"Received update request from {sender_ip} for group: {group_id}")
         self.db_manager.record_message(group_id, content, sender_ip)
         self.logger.info(f"Message received from {sender_ip} in group {group_id}")
+        self.logger.debug(f"Message content: {content[:50]}...") # Log first 50 characters of content
         
         if group_id in self.clients:
+            self.logger.debug(f"Processing update for clients in group {group_id}")
             for client in self.clients[group_id]:
                 if client != sender_ip:
                     if group_id not in self.pending_updates:
                         self.pending_updates[group_id] = {}
                     self.pending_updates[group_id][client] = content
+                    self.logger.debug(f"Queued update for client: {client}")
+        else:
+            self.logger.warning(f"Received update for non-existent group: {group_id}")
+        
+        self.logger.debug(f"Current pending updates: {self.pending_updates}")
         return jsonify({"status": "updated"}), 200
 
     def handle_poll(self, group_id):
         client_ip = request.remote_addr
+        self.logger.debug(f"Received poll request from {client_ip} for group: {group_id}")
+        
         if group_id in self.pending_updates and client_ip in self.pending_updates[group_id]:
             content = self.pending_updates[group_id].pop(client_ip)
+            self.logger.info(f"Sending update to client {client_ip} in group {group_id}")
+            self.logger.debug(f"Update content: {content[:50]}...") # Log first 50 characters of content
             if not self.pending_updates[group_id]:
                 del self.pending_updates[group_id]
+                self.logger.debug(f"Removed empty pending updates for group: {group_id}")
             return jsonify({"content": content}), 200
-        return jsonify({"content": None}), 200
+        else:
+            self.logger.debug(f"No pending updates for client {client_ip} in group {group_id}")
+            return jsonify({"content": None}), 200
 
 def create_server(logger):
     return Server(logger)
